dbbd28b9b7d198b0043b8e4b0c741c522e04bf48
Kristoffer Johansson <kristoffer.johansson@gaddr.com>
2022-03-14 14:59:34 +0100

    initial commit

---
+++ app/src/main/java/com/krillinator/navigation_controller_demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.krillinator.navigation_controller_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
